a0db5e1d44c223c57effd722cd0fae8f27b6178e
Listopad02 <123ayetvbass@gmail.com>
2022-09-06 16:57:15 +0300

    data mapping realized

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -1,37 +1,67 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, NavLink } from "react-router-dom";
 import Table from '../../components/Table/Table';
-import { useNavigate } from "react-router-dom";
 import api from '../../utils/api'
+import './Statistics.css'
 
 const Statistics = () => {
-  const navigate = useNavigate()
+    const navigate = useNavigate()
+    const [result, setResult] = useState('')
 
-  useEffect(() => {
-    if (!localStorage.getItem("access_token")) {
-      navigate("/")
-    }
-  })
+    useEffect(() => {
+      getStatistics()
+        if (!localStorage.getItem("access_token")) {
+          navigate("/")
+        }
+    }, [navigate])
 
-  async function getStatistics() {
+    async function getStatistics() {
       try {
         const response = await api(`/statistics`, {
           method: "GET",
           headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
+            Accept: "application/json",
+            "Content-Type": "application/json",
           },
         })
-        console.log('res', response)
+        setResult(response)
       } catch (err) {
         console.log(err)
       }
     }
 
-  return (
-    <div className='statistics'>
-      <Table />
-    </div>
-  )
+    console.log('res', result)
+
+    return (
+        <div className='statistics'>
+            <div className="statistics-container">
+                <div className="statistics-title">
+                    <h1 className='statistics-title__head'>Your account statistics</h1>
+                    <h2 className='statistics-title__text'>That table represents some detailed info: <br /> Original link, short link and number of clicks on each of the short links.</h2>
+                </div>
+                <div className="statistics-table__container">
+                  {
+                    result !== '' ?
+                    result.map((item) => {
+                      console.log('i', item)
+                      return (
+                        <div className="statistics-table">
+                          <div className="statistics-table__item">{item.counter}</div>
+                          <div className="statistics-table__item">{item.short}</div>
+                          <div className="statistics-table__item">{item.target}</div>
+                        </div>
+                      )
+                    }) : null
+                  }
+                </div>
+                <div className="statistics-logout">
+                    <p className='statistics-options__text'>
+                        Want to create some more short links? <NavLink to={'/service'}><span className='link'>Click</span></NavLink>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Statistics
