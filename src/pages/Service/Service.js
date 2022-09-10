c01111ddae7b5e54ca581e910e605b8b2e00f32c
Listopad02 <123ayetvbass@gmail.com>
2022-09-10 10:17:15 +0300

    new styles, markup and functionality

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate, NavLink } from "react-router-dom";
 import api from '../../utils/api'
 import './Service.css'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import copy from "../../img/copy.png"
 
 const Service = () => {
     const [link, setLink] = useState('')
     const [shortLink, setShortLink] = useState('')
+    const [open, setOpen] = useState(false)
+    const [success, setSuccess] = useState(true)
     const navigate = useNavigate()
+
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
 
     useEffect(() => {
         if (!localStorage.getItem("access_token")) {
@@ -36,6 +45,22 @@ const Service = () => {
         navigate('/')
     }
 
+    const handleClose = (reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        setOpen(false);
+    };
+
+    function copyLink() {
+        if (shortLink === '') {
+            setSuccess(false)
+        } else {
+            navigator.clipboard.writeText(shortLink)
+        }
+        setOpen(true)
+    }
+
     return (
         <div className='service'>
             <div className="service-container">
@@ -49,23 +74,37 @@ const Service = () => {
                     </p>
                 </div>
                 <div className="service-forms">
-                    <input type="text" 
-                           placeholder='Paste your link here' 
-                           className='service-forms__input'
-                           onChange={e => setLink(e.target.value)} />
+                    <div className="service-forms__container">
+                        <input type="text" 
+                            placeholder='Paste your link here' 
+                            className='service-forms__input'
+                            onChange={e => setLink(e.target.value)} />
+                    </div>
                     <button className='service-forms__button'
                     onClick={() => createShortLink()}>Create a link</button>
-                    <input type="text" 
-                           placeholder='Your short link will be here' 
-                           className='service-forms__input' 
-                           value={shortLink}
-                           readOnly />
+                    <div className="service-forms__copy">
+                        <input type="text" 
+                               placeholder='Your short link will be here' 
+                               className='service-forms__input' 
+                               value={shortLink}
+                               readOnly />
+                        <button className='service-forms__copy-button'
+                                onClick={() => copyLink()}>
+                            <img src={copy} alt="copy" />
+                        </button>
+                    </div>
+                    
                 </div>
                 <div className="service-logout">
                     <p className='login-options__text'>
                         Want to switch an account? <span className='link' onClick={() => logOut()}>Log out</span>
                     </p>
                 </div>
+                <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+                    <Alert severity={success ? 'success' : 'error'} sx={{ width: '100%' }}>
+                        {success ? 'Link have been copied!' : 'Field is empty!'}
+                    </Alert>
+                </Snackbar>
             </div>
         </div>
     )
